9628d6bf9343c7ac5bd5cbce0836cf7e3d02906a
VINAY <vinaybiradar1717>
2021-04-29 20:45:25 +0530

    read and update user profile

--- onlineVyapaar/routes/user.js
+++ onlineVyapaar/routes/user.js
@@ -2,16 +2,19 @@ const express = require("express")
 const router = express.Router()
 
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth")
-const { userById } = require("../controllers/user")
+const { userById, read, update } = require("../controllers/user")
 
 router.get("/secret/:userId", requireSignin, isAuth, isAdmin, (req, res) => {
     res.json({
         user: req.profile
-    })
+    });
 });
-
+// router.get("/user/:userId", requireSignin, isAuth, read); // shows user profile
+// router.put("/user/:userId", requireSignin, isAuth, update); // user can update his profile
+router.get('/user/:userId', requireSignin, isAuth, read);
+router.put('/user/:userId', requireSignin, isAuth, update);
 // act as a middleware
-router.param("userId", userById)
+router.param("userId", userById);
 
 
 module.exports = router;
